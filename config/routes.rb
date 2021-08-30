e692abcb64a4242d97d25c5dc47e31a583b61e45
oidaryou <oidaryou0311@gmail.com>
2021-08-30 06:01:05 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/about' => 'homes#about'
+  root  'homes#top'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+end
